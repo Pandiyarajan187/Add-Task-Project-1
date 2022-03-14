1e6afd6172721787547e944d379ac43b200df97f
Pandiyarajan <pandiyarajan.auctionsoftware@gmail.com>
2022-03-14 18:06:23 +0530

    now changes in DatePicker

--- src/Components/DatePicker.js
+++ src/Components/DatePicker.js
@@ -7,7 +7,7 @@ export default function TableDatePicker() {
  const [date, setDate] = useState(new Date());
 
  return (
-   <DatePicker  
+   <DatePicker 
    selected={date} 
    onChange={date => setDate(date)} />
  );
